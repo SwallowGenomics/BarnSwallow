99ce7a88050de53ddc2967809148696782e49ba1
Guido Gallo <64027918+GuidoGallo@users.noreply.github.com>
2021-12-02 11:34:44 +0100

    update script names

--- popgen_data_download_alignment/popgen_pipeline.sh.bash
+++ popgen_data_download_alignment/popgen_pipeline.sh.bash
@@ -1,5 +1,5 @@
 #This pipeline will download raw genomic data directly from NCBI public database, perform quality control and align them to the reference genome
-#usage: bash pipeline.sh Acclist.txt
+#usage: bash popgen_pipeline.sh Acclist.txt
 #Acclist.txt is a text file with all Accession numbers (SRR...) of the genomic data to download from the NCBI public archive
 
 
